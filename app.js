13917109b072cd683946cbad99e6a1b236fd4e64
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 09:07:58 -0300

    Variables, datos y operadores - ejercicio 9

---
+++ app.js
@@ -0,0 +1,7 @@
+//Kilómetros a millas
+
+//Crear un programa que pida ingresar una cantidad de kilómetros y muestre un mensaje con el resultado de la conversión en millas.
+
+
+const kilometros = prompt ("Por favor ingresa la cantidad de kilometros");
+alert("Conversión a millas: " + kilometros * 0.62 + " millas");
