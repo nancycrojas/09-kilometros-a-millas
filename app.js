9f0ada7b90a528499a9f6a4c25cd0190a806e1c0
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 20:15:39 -0300

    Agregando template strings

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 //Crear un programa que pida ingresar una cantidad de kilómetros y muestre un mensaje con el resultado de la conversión en millas.
 
+const kilometros = Number(prompt("Por favor ingresa la cantidad de kilometros"));
+//alert("Conversión a millas: " + kilometros * 0.62 + " millas");
 
-const kilometros = prompt ("Por favor ingresa la cantidad de kilometros");
-alert("Conversión a millas: " + kilometros * 0.62 + " millas");
+alert(`Conversión a millas: ${kilometros * 0.62} millas. `)
